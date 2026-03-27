962fb6d7a20a1110307042e0bb6c602c0f56d2aa
Bml2003 <beatriz.m.laranjinha@ubi.pt>
2026-03-27 22:52:17 +0000

    create_cat

--- programs/crypto_kitten/src/lib.rs
+++ programs/crypto_kitten/src/lib.rs
@@ -1,16 +1,48 @@
 use anchor_lang::prelude::*;
+//Importar a framework
 
-declare_id!("EzMSo5rEQjNAKwkFpuLRJ5Hna3JVruA55mnGMBiZv6wq");
+declare_id!("EzMSo5rEQjNAKwkFpuLRJ5Hna3JVruA55mnGMBiZv6wq"); // endereço da blockchain
 
+//contrato
 #[program]
 pub mod crypto_kitten {
+    //aquui dentro ficam instruções
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    //instrução da blockchain
+    // ctx contém tudo a que a função precisa : accounts , wallet e sistema
+    pub fn create_cat(ctx: Context<CreateCat>, name: String) -> Result<()> {
+        let cat = &mut ctx.accounts.cat_account; //aceder a conta onde o gato é guardado
+        cat.owner = ctx.accounts.user.key(); //guardar o dono
+        cat.name = name; //guardar o nome
+        cat.level = 1; //nivel inicial
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateCat<'info> {
+    //conta do gato
+    #[account(
+        init, //cria uma nova account
+        payer = user, //quem paga é o utilizador
+        space = 8 + Cat::INIT_SPACE //quanto espaço vai ocupar
+    )]
+    pub cat_account: Account<'info, Cat>,
+
+    #[account(mut)]
+    pub user: Signer<'info>, //quem esta a chamar a função tem de assinar a transação , é a wallet
+
+    pub system_program: Program<'info, System>, //para criar accounts na blockchain
+}
+
+//estrutura do gato
+//
+#[account]
+#[derive(InitSpace)]
+pub struct Cat {
+    pub owner: Pubkey, //dono do gato
+    #[max_len(50)]
+    pub name: String, //maximo de 50 caracteres
+    pub level: u8,     //nivel
+}
--- tests/crypto_kitten.ts
+++ tests/crypto_kitten.ts
@@ -3,14 +3,28 @@ import { Program } from "@coral-xyz/anchor";
 import { CryptoKitten } from "../target/types/crypto_kitten";
 
 describe("crypto_kitten", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.cryptoKitten as Program<CryptoKitten>;
+  const program = anchor.workspace.CryptoKitten as Program<CryptoKitten>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Creates a cat", async () => {
+    const catAccount = anchor.web3.Keypair.generate();
+
+    await program.methods
+      .createCat("Mimi")
+      .accounts({
+        catAccount: catAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([catAccount])
+      .rpc();
+
+    const catData = await program.account.cat.fetch(catAccount.publicKey);
+
+    console.log("Cat owner:", catData.owner.toBase58());
+    console.log("Cat name:", catData.name);
+    console.log("Cat level:", catData.level);
   });
 });
